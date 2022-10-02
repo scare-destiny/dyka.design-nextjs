879ee0095082625e3eb5224a6ff93110c5447ba5
scare-destiny <zhenya.venger@gmail.com>
2022-10-02 15:18:18 +0300

    updated id commercial image

--- pages/commercial/photos/[id].js
+++ pages/commercial/photos/[id].js
@@ -11,7 +11,6 @@ import Image from 'next/image'
 import Head from 'next/head'
 import Link from 'next/link'
 import { InfoIcon, AtSignIcon } from '@chakra-ui/icons'
-import { getPhotoById } from '../../api/api'
 import commercialImages from '../../../public'
 
 export async function getServerSideProps({ params }) {
@@ -27,39 +26,26 @@ export async function getServerSideProps({ params }) {
 export default function Photos({ picture }) {
 	console.log('test')
 	return (
-		<Box p='2rem' bg='white.700' minH='100vh'>
+		<Box p='2rem' bg='white.700' minH='100vh' al>
 			<Head>
 				<title> Dyka Design: {picture.title}</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 
 			<Flex px='1rem' justify='center' align='center'>
-				<Text
-					letterSpacing='wide'
-					textDecoration='underline'
-					// as='h2'
-					fontWeight='semibold'
-					fontSize='xl'
-					as='a'
-					target='_blank'
-					href={picture.photographer_url}
-				>
-					<AtSignIcon />
-					{picture.photographer}
-				</Text>
-				<Spacer />
-				<Box as='a' target='_blank' href={picture.url}>
-					<InfoIcon focusable='true' boxSize='2rem' color='red.500' />{' '}
-				</Box>{' '}
+			<Box>
+						<Link href='/'>Dyka Design</Link>
+					</Box>
+		
 				<Spacer />
 				<Link href={`/commercial`}>
 					<Button
 						as='a'
 						borderRadius='full'
-						mt={4} colorScheme='gray'
+				 colorScheme='gray'
 						type='submit'
 						fontSize='lg'
-						size='lg'
+						size='md'
 						cursor='pointer'
 					>
 						🏠  To Projects
@@ -71,8 +57,6 @@ export default function Photos({ picture }) {
 				<Box>
 					<Image
 						src={picture.image}
-						width={980}
-						height={700}
 						quality={50}
 						priority
 						loading='eager'
