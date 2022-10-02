37f7ffc76e27f528f7f13fe9a04a5032a8d5de59
scare-destiny <zhenya.venger@gmail.com>
2022-10-02 10:47:35 +0300

    revert blur

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -10,43 +10,11 @@ import { useState } from 'react'
 import Image from 'next/image'
 import styles from './About.module.css'
 
+
 const Portfolio = (props) => {
 	const { index, images, heading, subheading } = props
 
-	function cn(...classes) {
-		return classes.filter(Boolean).join(' ')
-	}
-
-	const BlurImage = ({ image }) => {
-		const [isLoading, setLoading] = useState(true)
-
-		return (
-			<WrapItem
-				key={index}
-				boxShadow='base'
-				rounded='20px'
-				overflow='hidden'
-				bg='white'
-				lineHeight='0'
-				_hover={{ boxShadow: 'dark-lg' }}
-			>
-				<Image
-					src={image.image}
-					key={index}
-					alt={image.title}
-					width={630}
-					height={450}
-					className={cn(
-						'duration-700 ease-in-out group-hover:opacity-75',
-						isLoading
-							? 'scale-110 blur-2xl grayscale'
-							: 'scale-100 blur-0 grayscale-0'
-					)}
-					onLoadingComplete={() => setLoading(false)}
-				/>
-			</WrapItem>
-		)
-	}
+	// TODO ADD BLUR
 
 	return (
 		<Box p={4} overflow='hidden' minH='100vh'>
@@ -71,25 +39,24 @@ const Portfolio = (props) => {
 				</chakra.h2>
 			</Container>
 			<Wrap px='1rem' spacing={4} justify='center'>
-				{images.map((image, index) => (
-					<BlurImage image={image} key={index} />
-					// <WrapItem
-					// 	key={index}
-					// 	boxShadow='base'
-					// 	rounded='20px'
-					// 	overflow='hidden'
-					// 	bg='white'
-					// 	lineHeight='0'
-					// 	_hover={{ boxShadow: 'dark-lg' }}
-					// >
-					// 	<Image
-					// 		src={image.image}
-					// 		key={index}
-					// 		alt={image.title}
-					// 		width={630}
-					// 		height={450}
-					// 	/>
-					// </WrapItem>
+				{images.map((picture, index) => (
+					<WrapItem
+						key={index}
+						boxShadow='base'
+						rounded='20px'
+						overflow='hidden'
+						bg='white'
+						lineHeight='0'
+						_hover={{ boxShadow: 'dark-lg' }}
+					>
+						<Image
+							src={picture.image}
+							key={index}
+							alt={picture.title}
+							width={630}
+							height={450}
+						/>
+					</WrapItem>
 				))}
 			</Wrap>
 		</Box>
