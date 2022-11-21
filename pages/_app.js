7224a60c7df1e51779653890616080bf656b7251
scare-destiny <zhenya.venger@gmail.com>
2022-11-21 19:34:37 +0200

    added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,15 @@
-
 // pages/_app.js
+import { Analytics } from '@vercel/analytics/react'
 import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  )
+	return (
+		<ChakraProvider>
+			<Component {...pageProps} />
+			<Analytics />
+		</ChakraProvider>
+	)
 }
 
 export default MyApp
